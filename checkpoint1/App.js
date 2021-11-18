90645574986a986adc5e1dc84b447d7036fe8b07
samSOUMAH <sam86soumah@gmail.com>
2021-11-18 12:13:28 +0100

    Ckekpoint Algo

---
+++ checkpoint1/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>ALGORITHME DE TRIE PAR INSERTION</h1>
+            <p>
+  En algorithme, le tri par insertion est un algorithme de tri classique.
+  En général, le tri par insertion est beaucoup plus lent que d'autres algorithmes comme:
+ -le tri rapide (ou quicksort)
+ -et le tri fusion pour traiter de grandes séquences. 
+ 
+ Car sa complexité asymptotique est quadratique.
+            </p>
+        </div>
+    )
+}
+
+export default App
