525dabf5e1b0d79a59ce7e843e4dfd6e1952618c
samSOUMAH <sam86soumah@gmail.com>
2021-11-27 23:58:32 +0100

    chekpoint JSX

--- checkpoint/mon-app/src/App.js
+++ checkpoint/mon-app/src/App.js
@@ -1,45 +1,20 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
-import './App.css';
+import './style.css';
+import avion3 from './avion3.jpg'
 
 function App() {
+  
   return (
-    <div className="d-flex container bg-warning">
-    <form >
- <fieldset>
-     <legend>Vos coordonnées</legend> 
-
-     <label for="nom">Quel est votre nom ?</label>
-     <input type="text" name="nom" id="nom" />
-
-     <label for="prenom">Quel est votre prénom ?</label>
-     <input type="text" name="prenom" id="prenom" />
-
-     <label for="email">Quel est votre e-mail ?</label>
-     <input type="email" name="email" id="email" />
-
- </fieldset>
-
- <fieldset>
-     <legend>Votre souhait</legend>
-
-     <p>
-         Faites un souhait que vous voudriez voir exaucé :
-
-         <input type="radio" name="souhait" value="riche" id="riche" /> <label for="riche">Etre riche</label>
-         <input type="radio" name="souhait" value="celebre" id="celebre" /> <label for="celebre">Etre célèbre</label>
-         <input type="radio" name="souhait" value="intelligent" id="intelligent" /> <label for="intelligent">Etre <strong>encore</strong> plus intelligent</label>
-         <input type="radio" name="souhait" value="autre" id="autre" /> <label for="autre">Autre...</label>
-     </p>
-
-     <p>
-         <label for="precisions">Si "Autre", veuillez préciser :</label>
-         <textarea name="precisions" id="precisions" cols="40" rows="4"></textarea>
-     </p>
- </fieldset>
-</form> 
-     
+    <div className="App">
+        <h1>Alkaly SOUMAH</h1>
+        <img src="/logo192.png" alt="image"/>
+        <img src={avion3} alt="logo"/>
+        <video width="420" height="340" controls>
+        <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" type=""/>
+        </video>
     </div>
   );
 }
