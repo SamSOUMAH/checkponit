7203473d5347bb93bcc5869c06700e6cd5654e7a
samSOUMAH <sam86soumah@gmail.com>
2021-12-04 20:03:00 +0100

    State checkpoint

--- checkpoint/mon-app/src/App.js
+++ checkpoint/mon-app/src/App.js
@@ -1,23 +1,22 @@
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import './style.css';
-import SousProfile from './Profil/SousProfile'
-
-function App() {
+import React from "react";
+class App extends React.Component {
+  person = {
+    fullname: "Alkaly SOUMAH",
+    bio:"je fais actuellement une formation a GoMyCode",
+    profession:"Etudiant",
+    imgSrc:"",
+    shows:false
+  };
   
-  return (
-    <div className="App">
-        <SousProfile
-        fullName={'Alkaly SOUMAH'}
-        Bio={"FullStack"}
-        Profesion={"staudent a Gomycode"}
-       
-        />
-        
+  render() {
+    return(
+    <div>
+      <div>fullname: <span>{this.person.fullname}</span></div>
+      <div>bio: <span>{this.person.bio}</span></div>
+      <div>profession: <span>{this.person.profession}</span></div>
+      <div>imgSrc: <span>{this.person.imgSrc}<img src="./profile.jpg" alt="photo" width="120px" height="120px"/></span></div>
     </div>
-  );
-}
-
+     )};
+ }
+ 
 export default App;
