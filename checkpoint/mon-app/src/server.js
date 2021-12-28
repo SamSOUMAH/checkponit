90a09754f89d523b30b6da8c1f28cb237ac94fc0
samSOUMAH <sam86soumah@gmail.com>
2021-12-29 00:28:52 +0100

    cheickpoint mongoDB  debut

---
+++ checkpoint/mon-app/src/server.js
@@ -0,0 +1,13 @@
+
+const  db  =  require ( 'db' ) 
+db . connect ( { 
+  hôte : processus . env . DB_HOST , 
+  nom d'utilisateur : processus . env . DB_USER , 
+  mot de passe : processus . env . DB_PASS 
+} )
+
+// EN ATTENDANT JE VAIS REVENIR FINALISER LE CHEICKPOINT
+
+DB_HOST =localhost
+DB_USER =root
+DB_PASS =s1mpl3
